df0fa935d125a4d1260209cedcd53cfeb20fd4cf
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-06-03 11:58:11 -0500

    added new js for fish filtering

--- scripts/fish/fishData.js
+++ scripts/fish/fishData.js
@@ -2,7 +2,7 @@ const fishCollection = [
     {
         name: "Carlene",
         species: "Mola Mola",
-        length: "6ft",
+        length: 6,
         location: "Atlantic Ocean",
         food: "Happiness",
         picture:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Sunfish2.jpg/1200px-Sunfish2.jpg"
@@ -10,7 +10,7 @@ const fishCollection = [
     {
         name: "Komal",
         species: "Anglerfish",
-        length: "3ft",
+        length: 3,
         location: "Deep Deep",
         food: "Steak Tartare",
         picture:"https://static01.nyt.com/images/2019/07/29/science/29SCI-ANGLERFISH-promo/29SCI-ANGLERFISH-promo-superJumbo.jpg"
@@ -18,7 +18,7 @@ const fishCollection = [
     {
         name: "Gruffydd",
         species: "Oarfish",
-        length: "8m",
+        length: 8,
         location: "Mesopelagic",
         food: "Unsliced Bread",
         picture:"https://external-preview.redd.it/pYMOEFuLKEhOw306jXYPyDAAkJFRdvxCbyjFBzi88eo.jpg?auto=webp&s=67fe3a904474205667a4be8d35d9d0c979fc369c"
@@ -26,7 +26,7 @@ const fishCollection = [
     {
         name: "Melody",
         species: "Blobfish",
-        length: "30cm",
+        length: 30,
         location: "Deep Deep",
         food: "Existentialism",
         picture:"https://elhstalon.net/wp-content/uploads/2019/09/Blobfish.jpg"
@@ -42,7 +42,7 @@ const fishCollection = [
     {
         name: "Rasmussen",
         species: "Axolotl",
-        length: "9in",
+        length: 9,
         location: "Mexico",
         food: "Palak Paneer",
         picture:"https://cdn.mos.cms.futurecdn.net/hKm2MuLdDLubD6rgeYLeDM-1200-80.jpg"
@@ -50,9 +50,46 @@ const fishCollection = [
     {
         name: "Ancro",
         species: "Whale Shark",
-        length: "33ft",
+        length: 33,
         location: "Dumpré",
         food: "C+ Bass",
         picture:"https://i.ytimg.com/vi/i-VMrW42J-0/maxresdefault.jpg"
     }
 ]
+
+
+// 3, 6, 9, 12, etc... fish
+const mostHolyFish = (fishObject) => {
+    const holyFish = []
+
+    for (const fishObject of fishCollection) {
+        if (fishObject.length % 3 === 0) {
+            holyFish.push(fishObject)
+        }
+    }
+    return holyFish
+}
+
+// 5, 10, 15, 20, 25, etc... fish
+const soldierFish = (fishObject) => {
+    const soldiers = []
+
+    for (const fishObject of fishCollection) {
+        if (fishObject.length % 5 === 0) {
+            soldiers.push(fishObject)
+        }
+    }
+    return soldiers
+}
+
+// Any fish not a multiple of 3 or 5
+const nonHolyFish = (fishObject) => {
+    const regularFish = [] 
+
+    for (fishObject of fishCollection) {
+        if (fishObject.length % 3 !== 0 && fishObject.length % 5 !== 0) {
+            regularFish.push(fishObject)
+        }
+    }
+    return regularFish
+}
